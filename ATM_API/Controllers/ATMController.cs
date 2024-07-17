4252ba399b131a9b9764f69ae6d546dc10096d30
prinsfries <162795069+prinsfries@users.noreply.github.com>
2024-07-17 22:47:27 +0800

    API

---
+++ ATM_API/Controllers/ATMController.cs
@@ -0,0 +1,57 @@
+﻿using atmBL;
+using atmDL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ATM_API.Controllers
+{
+    [ApiController]
+    [Route("api/user")]
+    public class ATMController : Controller
+    {
+        SqlDbData _SqlDbData;
+        public ATMController()
+        {
+            _SqlDbData = new SqlDbData();
+        }
+
+        [HttpGet("get-users")]
+        public IEnumerable<ATM_API.EWallet> GetUsers()
+        {
+            var ewUsers = _SqlDbData.GetEW();
+
+            List<ATM_API.EWallet> users = new List<EWallet>();
+
+            foreach (var item in ewUsers)
+            {
+                users.Add(new ATM_API.EWallet { Name = item.name, EWPin = item.EWPin });
+            }
+
+            return users;
+        }
+
+        [HttpPost("add-user")]
+        public JsonResult AddUser(EWallet request)
+        {
+            var result = _SqlDbData.AddUser(request.Name, request.EWPin);
+
+            return new JsonResult(result);
+        }
+
+        [HttpDelete("delete-user")]
+        public JsonResult DelUser(EWallet request)
+        {
+            var result = _SqlDbData.DelUser(request.EWPin);
+
+            return new JsonResult(result);
+        }
+
+        [HttpPatch("update-ewallet")]
+        public JsonResult UpdateUser(EWallet request)
+        {
+            var isUpdated = _SqlDbData.TransacDB(request.Money, request.EWPin);
+
+            return new JsonResult(new { Success = isUpdated });
+        }
+
+    }
+}
---
+++ ATM_API/EWallet.cs
@@ -0,0 +1,9 @@
+﻿namespace ATM_API
+{
+    public class EWallet
+    {
+        public String Name { get; set; }
+        public String EWPin { get; set; }
+        public int Money { get; set; }
+    }
+}
