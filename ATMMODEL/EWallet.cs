cc668e6a5e19ddb382da433186bf244b71efbe0f
prinsfries <162795069+prinsfries@users.noreply.github.com>
2024-07-17 23:04:51 +0800

    Add files via upload

---
+++ ATMMODEL/EWallet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMMODEL
+{
+    public class EWallet
+    {
+        public string name;
+        public string EWPin;
+        public int money;
+    }
+}
